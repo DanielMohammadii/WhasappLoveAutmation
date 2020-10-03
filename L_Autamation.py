d8ba2d421e7dccdd56ae26e6688902442b56da8a
Dani <road2product@gmail.com>
2020-10-03 17:28:14 +0200

    First inti

--- L_Autamation.py
+++ L_Autamation.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client 
  
-account_sid = 'ACa7a4b83e2896b6ac63584763048c6ccc' 
-auth_token = '12072c34cbd497c6cf2082048fd18074' 
+account_sid = 'your-sid' 
+auth_token = 'Auth-token' 
 client = Client(account_sid, auth_token) 
 
 def send_love(): 
     message = client.messages.create( 
-                                from_='whatsapp:+14155238886',  
+                                from_='whatsapp:+14111111',  
                                 body='This is Daniel\'s robot',      
                                 to='whatsapp:+393892551792' 
                             ) 
